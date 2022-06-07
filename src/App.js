3c6001e78d89cfc1e8247423946c9461e6cde507
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-07 16:32:12 -0300

    feat: Renderiza form de filtros

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
+import FilterForm from './components/FilterForm';
 import './App.css';
 
 function App() {
   return (
     <PlanetsProvider>
+      <FilterForm />
       <Table />
     </PlanetsProvider>
   );
