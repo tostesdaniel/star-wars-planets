8517bb7bd03558941d940aeaed0b722ed2a47811
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-07 15:49:48 -0300

    feat: Cria Table

---
+++ src/components/Table.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+export default function Table() {
+  const { data } = useContext(PlanetsContext);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map(({
+            name,
+            rotation_period: rotationPeriod,
+            orbital_period: orbitalPeriod,
+            diameter,
+            climate,
+            gravity,
+            terrain,
+            surface_water: surfaceWater,
+            population,
+            films,
+            created,
+            edited,
+            url,
+          }) => (
+            <tr key={ name }>
+              <td>{name}</td>
+              <td>{rotationPeriod}</td>
+              <td>{orbitalPeriod}</td>
+              <td>{diameter}</td>
+              <td>{climate}</td>
+              <td>{gravity}</td>
+              <td>{terrain}</td>
+              <td>{surfaceWater}</td>
+              <td>{population}</td>
+              <td>{films}</td>
+              <td>{created}</td>
+              <td>{edited}</td>
+              <td>{url}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+}
