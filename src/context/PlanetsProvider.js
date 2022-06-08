32dae5a53cd09be8ffae100abd694cf5fd727007
Daniel Tostes <ohtosttnz@gmail.com>
2022-06-07 21:42:10 -0300

    feat: Cria um segundo array e o state dos filtros

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
+// ---Mock---
+// import response from '../testData';
 
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [filterByName, setFilterByName] = useState('');
+  const [filterByNumericValues, setFilterByNumericValues] = useState([
+    {
+      column: 'population',
+      comparison: 'maior que',
+      value: 0,
+    },
+  ]);
+
   const context = {
     data,
     setData,
+    filteredData,
+    setFilteredData,
     filterByName,
     setFilterByName,
+    filterByNumericValues,
+    setFilterByNumericValues,
   };
 
   async function fetchPlanets() {
     const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
-    const response = await fetch(url);
-    const apiData = await response.json();
+    const apiResponse = await fetch(url);
+    const apiData = await apiResponse.json();
     const { results } = apiData;
     setData(results);
+
+    // ---Mock---
+    // setData(response.results);
   }
 
   useEffect(() => {
-    fetchPlanets();
-  }, []);
+    if (!data.length) {
+      fetchPlanets();
+    }
+    setFilteredData(data);
+  }, [data]);
 
   return (
     <PlanetsContext.Provider value={ context }>
