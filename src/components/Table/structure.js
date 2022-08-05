7717cdfbc61e65d2ebd922902a3fa4caa805b134
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-04 23:08:05 -0300

    feat: exports new object with table column names

---
+++ src/components/Table/structure.js
@@ -0,0 +1,19 @@
+const tableData = {
+  column: [
+    'Name',
+    'Rotation Period',
+    'Orbital Period',
+    'Diameter',
+    'Climate',
+    'Gravity',
+    'Terrain',
+    'Surface Water',
+    'Population',
+    'Films',
+    'Created',
+    'Edited',
+    'URL',
+  ],
+};
+
+export default tableData;
