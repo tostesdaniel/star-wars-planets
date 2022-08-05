c5ed0c4fa231a11c296e6a9b7c6ea16f5af70694
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-04 23:12:11 -0300

    feat: renders Header and imports index.css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
 import FilterForm from './components/FilterForm';
 import './App.css';
+import './index.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <PlanetsProvider>
+      <Header />
       <FilterForm />
       <Table />
     </PlanetsProvider>
