4963c32f33dd46b93d9a7eca2aa64b33e8da4843
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-20 13:34:02 -0300

    feat: remove appliedFilters element and deps

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,17 +1,11 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useContext, useState } from 'react';
-import {
-  SearchIcon,
-  FilterIcon,
-  TrashIcon,
-  MinusCircleIcon,
-} from '@heroicons/react/solid';
+import React, { useContext } from 'react';
+import { SearchIcon, FilterIcon, TrashIcon } from '@heroicons/react/solid';
 import PlanetsContext from '../context/PlanetsContext';
 import SortForm from './SortForm';
+import AppliedFilters from './AppliedFilters';
 
 export default function FilterForm() {
-  const [appliedFilters, setAppliedFilters] = useState([]);
-
   const {
     data,
     filteredData,
@@ -21,6 +15,8 @@ export default function FilterForm() {
     setFilterByNumericValues,
     availableFilters,
     setAvailableFilters,
+    appliedFilters,
+    setAppliedFilters,
   } = useContext(PlanetsContext);
 
   const inputHandler = ({ target: { value: val } }, type, property) => {
@@ -88,44 +84,6 @@ export default function FilterForm() {
       'surface_water',
     ];
     setAvailableFilters(originalFilters);
-  };
-
-  const updateFilterList = (currAppliedFilters) => {
-    const originalFilters = [
-      'population',
-      'orbital_period',
-      'diameter',
-      'rotation_period',
-      'surface_water',
-    ];
-    const newAvailableFilters = originalFilters.filter(
-      (filter) => !currAppliedFilters.includes(filter),
-    );
-    setAvailableFilters(newAvailableFilters);
-  };
-
-  const updateFilters = (currAppliedFilters) => {
-    let newData = [...data];
-    currAppliedFilters.forEach(({ property, operator, value }) => {
-      if (operator === 'menor que') {
-        newData = newData.filter((planet) => planet[property] < Number(value));
-      }
-      if (operator === 'maior que') {
-        newData = newData.filter((planet) => planet[property] > Number(value));
-      } else {
-        newData = newData.filter((planet) => planet[property] === value);
-      }
-    });
-    setFilteredData(newData);
-    updateFilterList(currAppliedFilters);
-  };
-
-  const removeAppliedFilter = (filterToRemove) => {
-    const newAppliedFilters = appliedFilters.filter(
-      ({ property }) => property !== filterToRemove,
-    );
-    setAppliedFilters(newAppliedFilters);
-    updateFilters(newAppliedFilters);
   };
 
   const resetTable = () => {
@@ -259,24 +217,7 @@ export default function FilterForm() {
       </form>
       <div className="flex items-center gap-4 px-12">
         <SortForm />
-        {appliedFilters.map(({ property, operator, value }, index) => (
-          <p
-            key={ `appliedFilter-${index}` }
-            data-testid="filter"
-            className="mr-4 mt-9 inline-flex border-b border-slate-400 pb-2 align-middle
-            font-semibold hover:border-slate-300 hover:text-slate-300"
-          >
-            {`${property} ${operator} ${value}`}
-            <button
-              type="button"
-              name="remove-filter-button"
-              id="remove-filter-button"
-              onClick={ () => removeAppliedFilter(property) }
-            >
-              <MinusCircleIcon className="ml-2 mt-1 h-4 w-4 text-red-800 hover:text-red-600" />
-            </button>
-          </p>
-        ))}
+        <AppliedFilters />
       </div>
     </>
   );
